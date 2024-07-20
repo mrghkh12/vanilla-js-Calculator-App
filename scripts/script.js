8f77ee8dccf6de57250efe7d43976abdc929f1d5
mohammad reza <mrghkh12@gmail.com>
2024-07-20 12:29:43 +0330

    feat(script.js): set event in operator & number btn

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,23 @@ const calculatorDisplay = $.querySelector('.dasplay')
 
 const operatorsBtn = $.querySelectorAll('.operator')
 const numbersBtn = $.querySelectorAll('.number')
+
+numbersBtn.forEach(number => {
+    number.addEventListener('click', e => {
+        calculatorDisplay.value += e.target.value
+    })
+})
+
+operatorsBtn.forEach(operator => {
+    operator.addEventListener('click', e => {
+        e.target.value == 'AC' && (calculatorDisplay.value = '')
+        e.target.value == 'DE' && (calculatorDisplay.value = calculatorDisplay.value.toString().slice(0,-1))
+        e.target.value == '.' && (calculatorDisplay.value += '.')
+        e.target.value == '/' && (calculatorDisplay.value += '/')
+        e.target.value == '*' && (calculatorDisplay.value += '*')
+        e.target.value == '-' && (calculatorDisplay.value += '-')
+        e.target.value == '+' && (calculatorDisplay.value += '+')
+        e.target.value == '=' && (calculatorDisplay.value = eval(calculatorDisplay.value))
+
+    })
+})
