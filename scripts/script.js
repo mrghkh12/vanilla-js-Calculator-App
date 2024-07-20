01195139f2a14bc5219f6cd9ef13aabe20650b7d
mohammad reza <mrghkh12@gmail.com>
2024-07-20 12:42:07 +0330

    feat: fix error msg

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,14 @@
 const $ = document
 
 const calculatorDisplay = $.querySelector('.dasplay')
+const errorLabel = $.querySelector('.displayBox .error')
 
 const operatorsBtn = $.querySelectorAll('.operator')
 const numbersBtn = $.querySelectorAll('.number')
 
 numbersBtn.forEach(number => {
     number.addEventListener('click', e => {
+        errorLabel.style.display = 'none'
         calculatorDisplay.value += e.target.value
     })
 })
@@ -20,7 +22,17 @@ operatorsBtn.forEach(operator => {
         e.target.value == '*' && (calculatorDisplay.value += '*')
         e.target.value == '-' && (calculatorDisplay.value += '-')
         e.target.value == '+' && (calculatorDisplay.value += '+')
-        e.target.value == '=' && (calculatorDisplay.value = eval(calculatorDisplay.value))
+        e.target.value == '=' && (calculatorDisplay.value = calculation(calculatorDisplay.value))
 
     })
 })
+
+function calculation(value){
+    try{
+        return eval(value)
+    }catch{
+        errorLabel.style.display = 'block'
+        return ''
+    }
+    
+}
